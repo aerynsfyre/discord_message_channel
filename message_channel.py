6832017919398b8a10462350da00c1b93e6cc3a3
Aeryn Dunmore <113153786+aerynsfyre@users.noreply.github.com>
2024-12-30 09:42:11 +1300

    Update message_channel.py

--- message_channel.py
+++ message_channel.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python3
 import requests
 
-channel_url = "https://discord.com/api/v9/channels/1271346196069486676/messages"
-my_channel = "https://discord.com/api/v9/channels/1271397067490852978/messages"
-juliet_channel = "https://discord.com/api/v9/channels/1271397067490852978/messages"
-bot_token = "MTI3MTM0NDMxOTYwNDg1MDcxMQ.GVQq70.psKOL-8vtz_TRGlIfZy6ylldbcrDDB7gicihhg"
-user_id = "726601126161547384"
-juliet_id = "1271368126482616342"
+channel_url = "https://discord.com/api/v9/channels/<YOUR-CHANNEL-ID-HERE>/messages"
+bot_token = "<BOT-ACCOUNT-AUTHORISATION-TOKEN-HERE>"
+user_id = "<YOUR-USER-ID-HERE>"
 
 def send_msg(message):
 	payload = {
