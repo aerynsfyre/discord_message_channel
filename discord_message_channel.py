ab06d1efc75f6cc2a4fb344dfda5d7f26d231dc1
Aeryn Dunmore <113153786+aerynsfyre@users.noreply.github.com>
2024-12-30 17:08:40 +1300

    Update discord_message_channel.py

--- discord_message_channel.py
+++ discord_message_channel.py
@@ -1,16 +1,77 @@
 #!/usr/bin/env python3
-import requests
+# Change channel, token, and user id. This can be done via python discord_message_channel set_auth <token> <channel_id> <user_id>
+# Token requires the creation of another Discord account to send your messages.
+# Channel should be a text channel on your server.
+# User is the ID of the account which will be mentioned as @ in the message.
 
-channel_url = "https://discord.com/api/v9/channels/<YOUR-CHANNEL-ID-HERE>/messages"
-bot_token = "<BOT-ACCOUNT-AUTHORISATION-TOKEN-HERE>"
-user_id = "<YOUR-USER-ID-HERE>"
+import requests
+import pickle
+
+global auth
+auth = {
+"token": "<your-token>",
+"channel":"https://discord.com/api/v9/channels/<your-channel-url>/messages",
+"user": "<your-user-id>",
+}
+
+def get_auth():
+	global auth
+	with open("auth.pickle","rb") as auth_info:
+		auth = pickle.load(auth_info)
+	return auth
+
+def write_auth():
+	global auth
+	with open("auth.pickle","wb") as a:
+		pickle.dump(auth)
+		
+def set_auth(values):
+	global auth
+	auth["token"] = values[0]
+	auth["channel"] = values[1]
+	auth["user"] = values[2]
+	write_auth()
 
 def send_msg(message):
+	global auth
+	auth = get_auth()
 	payload = {
-		"content":str(f"<@{user_id}> Program Notification: {message}")
+		"content":str(f"<@{auth['user']}> Program Notification: {message}")
 	}
 	headers = {
-		"authorization":bot_token
+		"authorization":auth["token"]
 	}
-	res = requests.post(my_channel,payload,headers=headers)
+	res = requests.post(auth["channel"],payload,headers=headers)
 	
+def update_token(token):
+	global auth
+	auth = get_auth()
+	auth["token"] = token
+	write_auth()
+	
+def update_channel(channel_new):
+	global auth
+	auth = auth = get_auth()
+	auth["channel"] = channel_new
+	write_auth()
+	
+def update_user(user_new):
+	global auth
+	auth = get_auth()
+	auth["user"] = user_new
+	write_auth()
+	
+def get_channel():
+	global auth
+	auth = get_auth()
+	return auth["channel"]
+
+def get_token():
+	global auth
+	auth = get_auth()
+	return auth["token"]
+
+def get_user():
+	global auth
+	auth = get_auth()
+	return auth["user"]
